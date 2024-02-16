9a7e2b0ce0f180f440b1e6871781667d55665e52
xiao.wenpeng <ja_xwp@yeah.net>
2024-02-16 13:37:20 +0900

    添加nacos支持

--- batch/src/main/java/com/jiawa/train/batch/feign/BusinessFeign.java
+++ batch/src/main/java/com/jiawa/train/batch/feign/BusinessFeign.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Date;
 
- @FeignClient(name = "train-ticket-business", url = "http://127.0.0.1:8002")
+// @FeignClient(name = "train-ticket-business", url = "http://127.0.0.1:8002")
+@FeignClient("business")
 public interface BusinessFeign {
 
-    @GetMapping("business/admin/daily-train/gen-daily/{date}")
+    @GetMapping("/business/admin/daily-train/gen-daily/{date}")
     JsonResult genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
 }
